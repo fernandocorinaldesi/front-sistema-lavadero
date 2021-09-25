04c0d0def73b861eb8d4d84d7781bfb5743ae66f
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2021-09-25 13:18:21 -0300

    Se creó el footer

---
+++ src/components/core/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-footer padless absolute>
+    <v-row justify="center" no-gutters>
+      <v-col class="primary py-4 text-center white--text" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Sistema Lavadero</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "DashboardCoreFooter",
+  data: () => ({
+    links: ["Home", "About Us", "Team", "Services", "Blog", "Contact Us"],
+  }),
+};
+</script>
