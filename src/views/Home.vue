8a15dcbfba4ee28450af4f04a1078fdf87aec85c
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2021-09-25 13:23:38 -0300

    Se creo la vista del home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Wellcome/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Wellcome from '@/components/Wellcome.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Wellcome
   }
 }
 </script>
