866207f761317746ef56d6f1156025eae739c727
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2021-09-25 13:18:37 -0300

    Se crearon las rutas iniciales

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,29 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
+export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/views/Index'),
+      children: [
+        // Dashboard
+        {
+          name: 'Main',
+          path: '',
+          component: () => import('@/views/Home'),
+        },
+        {
+          name: 'About',
+          path: '/About',
+          component: () => import('@/views/About'),
+        },
 
-export default router
+      ],
+    },
+  ],
+})
