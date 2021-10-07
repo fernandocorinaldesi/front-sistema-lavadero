58cfe1eeaabe49edc179042ba40bf45c6bf943c1
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2021-10-07 18:57:41 -0300

    Se creó el componente para alta de cliente

---
+++ src/components/AddClient.vue
@@ -0,0 +1,115 @@
+<template>
+  <v-container>
+    <v-row class="text-center justify-center align-center mt-4 mb-5">
+      <v-col cols="8" class="elevation-2">
+          <h2 class="headline mb-3">Formulario de alta de cliente</h2>
+        <v-form ref="form" v-model="valid" lazy-validation class="mb-5">
+          <v-row>
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="firstname"
+                :rules="nameRules"
+                :counter="10"
+                label="Nombre"
+                required
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="lastname"
+                :rules="nameRules"
+                :counter="10"
+                label="Apellido"
+                required
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                label="E-mail"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="firstname"
+                :rules="nameRules"
+                :counter="10"
+                label="DNI"
+                required
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="lastname"
+                :rules="nameRules"
+                :counter="10"
+                label="Direccion"
+                required
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="lastname"
+                :rules="nameRules"
+                label="Telefono"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+              <div class="ml-3 mt-3">
+              <v-btn
+                :disabled="!valid"
+                color="success"
+                class="mr-4"
+                @click="validate"
+                >Ingresar</v-btn
+              >
+              <v-btn color="error" class="mr-4" @click="reset">Descartar</v-btn>
+              </div>
+          
+          </v-row>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "AddClient",
+  data: () => ({
+    valid: false,
+    firstname: "",
+    lastname: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => v.length <= 10 || "Name must be less than 10 characters",
+    ],
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+/.test(v) || "E-mail must be valid",
+    ],
+  }),
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    },
+  },
+};
+</script>
