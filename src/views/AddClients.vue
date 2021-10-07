8aeb0eb57de26eb5c23b9c95959b4bfb25d3a914
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2021-10-07 18:59:29 -0300

    Se creó la vista para alta de cliente

---
+++ src/views/AddClients.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="AddClient">
+    <AddClient/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AddClient from '@/components/AddClient.vue'
+
+export default {
+  name: 'AddClients',
+  components: {
+    AddClient
+  }
+}
+</script>
