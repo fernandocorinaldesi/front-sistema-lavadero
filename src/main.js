1cb065bc5932383d5d42d6eafd450b2f7aed8a90
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2021-11-11 11:58:00 -0300

    Se agregaron las dependencias del v-calendar

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import store from './store/store'
+import VCalendar from 'v-calendar';
+
+
+Vue.use(VCalendar, {
+  componentPrefix: 'vc',  // Use <vc-calendar /> instead of <v-calendar />
+           // ...other defaults
+});
 
 Vue.config.productionTip = false
 
